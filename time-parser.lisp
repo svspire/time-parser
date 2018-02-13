b62a6d9b6f44347afd667d941eaed0edd9088c05
Shannon Spires <ssos@bearlanding.com>
2018-02-13 10:19:10 -0700

    Added in-package.

--- time-parser.lisp
+++ time-parser.lisp
@@ -21,6 +21,8 @@
 ;;; Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
 ;;; USA
 
+(in-package :time-parser)
+ 
 (export '*default-day*)
 (export '*default-hour*)
 (export '*default-language*)
